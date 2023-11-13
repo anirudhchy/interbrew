f9354fbdee7156ebc43d170d7bb2b52fad438d74
Aniruddha Chaudhary <aniruddha@Aniruddhas-MacBook-Air.local>
2023-11-13 22:28:19 +0530

    Added theme provider and theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-60">
-        <Input placeholder="Email" />
-        <Button className="mt-3" variant="outline">
-          Submit
-        </Button>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-24">
+      <ModeToggle />
+
+      <Input className="w-60" placeholder="Email" />
+      <Button variant="outline">Submit</Button>
     </main>
   );
 }
